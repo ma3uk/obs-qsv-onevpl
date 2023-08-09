ef667157f9f839574d1e074b7c98aed50bd10fd8
ma3uk <m.gudoff@yandex.ru>
2023-08-09 20:43:12 +0600

    Delete linux_defs.h

--- obs-qsvonevpl/bits/linux_defs.h
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include <string.h>
-#include <time.h>
-
-#define MSDK_FOPEN(FH, FN, M)           { FH=fopen(FN,M); }
-#define MSDK_SLEEP(X)                   { usleep(1000*(X)); }
-
-typedef timespec mfxTime;
